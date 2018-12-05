ee435bce5d640dbe2cfaedfe9809ac9689578977
Ddd <752042257@qq.com>
2018-12-05 18:28:18 +0800

    register

--- hw3/after_end/model.py
+++ hw3/after_end/model.py
@@ -27,23 +27,13 @@ def do_comment_filter(param):
 
 def do_register(param):
     name = param['name']
-    print(name)
     res = requests.get('http://119.23.241.119:8080/Entity/U3306a6d35762f/TNS/User/?User.name='+name)
-    print(res.text)
     json_res = json.loads(res.text)
-    print(json_res)
     rt = ""
-    print(len(json_res))
     if len(json_res)==0:
-        print("post to db")
-        print(param)
         _param = json.dumps(param)
-        print(_param)
         tmp = requests.post('http://119.23.241.119:8080/Entity/U3306a6d35762f/TNS/User/', _param)
-        print(tmp)
         rt = json.loads(tmp.text)
-        print(rt)
-        print(rt['id'])
         return rt['id']
     elif name == json_res['User'][0]['name']:
         return rt
